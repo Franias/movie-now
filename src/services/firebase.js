2d76dc958ffca7734bbcfc02324ccb962f9ffbd9
franias <franciellipdias@gmail.com>
2023-11-28 20:53:40 -0300

    corrige firebaseConfig

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnvE6GdYsR698ZHwSeXXU0MXYThSwp4J4",
+  authDomain: "project-movie-now.firebaseapp.com",
+  projectId: "project-movie-now",
+  storageBucket: "project-movie-now.appspot.com",
+  messagingSenderId: "326794518728",
+  appId: "1:326794518728:web:5ddba2823a37c0e91ec5fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
