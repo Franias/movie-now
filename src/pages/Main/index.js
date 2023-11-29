6ddfe9a68122addf89b343bcfca94b8c072e7d17
franias <franciellipdias@gmail.com>
2023-11-28 21:13:44 -0300

    alterando o main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,40 @@
 import React, {Component} from "react";
-
+import {Row, Col, Button,Container, Card, } from 'react-bootstrap';
 class index extends Component {
-    state = {  }
+    state = {};
     render() {
-        return <h1>Main </h1>;
+        return(
+        <>
+        <Container
+            style={{
+                paddingTop:50,
+                display: 'flex',
+                flexDirection:'column',
+                alignItens: 'center',
+                minWidth: 300,
+            }}
+            >
+            <h1 style={{
+                color: '#aaaa',
+                marginTop: 20
+            }}>Main</h1>
+            <Row>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
+            </Row>
+            
+        </Container>
+        
+        </>);
     }
 }
 
