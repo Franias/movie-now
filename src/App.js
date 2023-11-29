7f50e97175861f9e7123a1e3bde6194adec5811a
franias <franciellipdias@gmail.com>
2023-11-28 23:09:34 -0300

    criando styles no main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import Main from './pages/Main';
+import Main from './pages/Main/index';
 function App() {
   return < Main />;
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,60 +1,45 @@
-import React, {Component} from "react";
-import {Row, Col, Button,Container, Card, } from 'react-bootstrap';
-class Main extends Component {
-    state = {};
-    render() {
-        return(
-        <>
-        <Container
-            style={{
-                paddingTop:50,
-                display: 'flex',
-                flexDirection:'column',
-                alignItens: 'center',
-                minWidth: 300,
-            }}
-            >
-            <h1 style={{
-                color: '#aaaa',
-                marginTop: 20,
-                fontFamily: "ubuntu",
-                alignItens: 'center',
-                alignSelf: 'center',
-            }}>Movie Now</h1>
-            <Row style={{
+import React from 'react';
+import Header from '../../components/Header';
+import {StyledContainer, StyledButton, StyledRow, StyledCard} from './styles'
+function Main() {
+    return <>
+    <Header />
+    
+
+    <StyledContainer>
+            <h1>Movie Now</h1>
+            <StyledRow style={{
                 display: 'flex', justifyContent: 'center'
             }}>
-            <Card style={{ width: 250, margin : 10 }}>
-                <Card.Img style={{
+            <StyledCard style={{ width: 250, margin : 10 }}>
+                <StyledCard.Img style={{
                     width: 180,
                     height: 180,
                     alignSelf: 'center',
                     paddingTop:20,
                     cursor: 'pointer',
                 }} variant="top" src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*JzDt_DAFQRA_uXMzSAkM7g.jpeg" />
-                <Card.Body style={{
+                <StyledCard.Body style={{
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'space-between'
                 }}>
-                    <Card.Title>Insecure </Card.Title>
-                    <Card.Text style={{
+                    <StyledCard.Title>Insecure </StyledCard.Title>
+                    <StyledCard.Text style={{
                         fontSize: 20
                     }}>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text>
-                    <Button style={{
+                    Some quick example text to build on the StyledCard title and make up the
+                    bulk of the StyledCard's content.
+                    </StyledCard.Text>
+                    <StyledButton style={{
                         width:200
-                    }} variant="outline-dark">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-            </Row>
+                    }} variant="outline-dark">Go somewhere</StyledButton>
+                </StyledCard.Body>
+                </StyledCard>
+            </StyledRow>
             
-        </Container>
-        
-        </>);
-    }
+        </StyledContainer>
+    </>
 }
 
 export default Main;
