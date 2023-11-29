c341ab40797a8a16876a318399afabd1ee0d5317
franias <franciellipdias@gmail.com>
2023-11-29 04:21:57 -0300

    erro no routes continua

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
 import Main from './../src/pages/Main'
 import ListEpisodes from './../src/pages/ListEpisodes'
 import Watch from './../src/pages/Watch'
@@ -6,11 +6,11 @@ const MyRoutes = () => (
     <BrowserRouter>
     <Routes>
 
-        <Route path= "/" exact element={<Main/>}/>
-        <Route path= "/list-episodes" element={<ListEpisodes/>}/>
-        <Route path= "/watch" element={<Watch/>}/>
-    </Routes>
+        <Route exact path= "/"  element={<Main/>}/>
+        <Route exact path= "/list-episodes" element={<ListEpisodes/>}/>
+        <Route exact path= "/watch" element={<Watch/>}/>
 
+    </Routes>
     </BrowserRouter>
 )
 export default MyRoutes;
